d50747e53e42f8be87c058fe86b89ddbba91ac04
a-staab <astaab@gmail.com>
2017-10-25 15:30:50 -0700

    Works for cyclical graphs

--- breadth_first.py
+++ breadth_first.py
@@ -1,23 +1,28 @@
-class Node(object):
+class GraphNode(object):
     def __init__(self, data, children=None):
         self.data = data
         self.children = children or []
 
-    def add_node(self, parent, value, children=None):
-        """Given the parent node, children, and value of a new node, creates a
-        new node and adds it to the graph."""
+    def add_new_node(self, parent, value):
+        """Given the parent node and value of a new node, creates a new node and
+        adds it to the graph."""
 
-        self = Node(value, children)
+        self = GraphNode(value)
         parent.children.append(self)
+
+    def make_cycle(self, parent, node):
+        """Create an edge connecting node to parent."""
+
+        parent.children.append(node)
 
 
 def find(node, value):
-    """Given a node in a directed graph (that may contain cycles) and a value,
+    """Given a node in a directed graph that may contain cycles and a value,
     finds the node in the graph with the corresponding value via breadth-first
     search and returns it.
 
     """
-    seen = {}
+    seen = set([])
     to_check = []
 
     if node.data == value:
@@ -28,14 +33,54 @@ def find(node, value):
 
     while to_check:
 
-        for current in to_check:
-            if current not in seen:
-                if current.data == value:
-                    return current
-                else:
-                    seen.add(current)
-                    to_check.extend(current.children)
+        current = to_check[0]
+
+        if current not in seen:
+            if current.data == value:
+                return current
             else:
-                continue
+                seen.add(current)
+                to_check.pop(0)
+                to_check.extend(current.children)
+
+        else:
+            to_check.remove(current)
 
     return "Node not found."
+
+"""For creating a test graph:
+
+Without cycles:
+
+graph = GraphNode("a")
+graph.add_new_node(graph, "b")
+graph.add_new_node(graph, "c")
+graph.children[0].add_new_node(graph.children[0], "d")
+graph.children[0].add_new_node(graph.children[0], "e")
+graph.children[0].add_new_node(graph.children[0], "f")
+graph.children[1].add_new_node(graph.children[1], "c")
+graph.children[1].children[0].add_new_node(graph.children[1].children[0], "g")
+
+Test cases:
+
+find(graph, "g")
+find(graph, "moon")
+
+With a cycle:
+
+graph = GraphNode("a")
+graph.add_new_node(graph, "b")
+graph.add_new_node(graph, "c")
+graph.children[0].add_new_node(graph.children[0], "d")
+graph.children[0].add_new_node(graph.children[0], "e")
+graph.children[0].add_new_node(graph.children[0], "f")
+graph.children[0].make_cycle(graph.children[0], graph)
+graph.children[1].add_new_node(graph.children[1], "c")
+graph.children[1].children[0].add_new_node(graph.children[1].children[0], "g")
+
+Test cases:
+
+find(graph, "g")
+find(graph, "moon")
+
+"""
